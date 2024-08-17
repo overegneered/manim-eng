ebfab9a2c06df1d784b420b0fa070503a7d62999
overegneered <git@egneer.ing>
2024-08-17 20:12:48 +0100

    #1 Add vector utils
    A few utils were added for basic operations on manim vectors not provided by numpy, along with tests. These are to be used in the placement of labels, hence their introduction.

---
+++ src/manim_eng/_utils/__init__.py
@@ -0,0 +1,78 @@
+import manim.typing as mnt
+import numpy as np
+
+
+def normalised(vector: mnt.Vector3D) -> mnt.Vector3D:
+    """Normalises the passed vector, and returns the result.
+
+    Parameters
+    ----------
+    vector : Vector3D
+        The vector to normalise.
+
+    Returns
+    -------
+    Vector3D
+        The resultant normalised vector.
+    """
+    return vector / np.linalg.norm(vector)
+
+
+def small_values_zeroed_out(
+    vector: mnt.Vector3D, tolerance: float = 0.0001
+) -> mnt.Vector3D:
+    """Return the input vector with smaller than ``tolerance`` zeroed-out.
+
+    Parameters
+    ----------
+    vector : Vector3D
+        The vector to start with.
+    tolerance : float
+        The maximum value for an element in ``vector`` that won't be set to zero.
+
+    Returns
+    -------
+    Vector3D
+        The resultant vector.
+    """
+    return vector * (np.abs(vector) > tolerance)
+
+
+def cardinalised(vector: mnt.Vector3D, margin: float) -> mnt.Vector3D:
+    """If ``vector`` is within ``margin`` of a cardinal direction, snap it to it.
+
+    The angle the passed ``vector`` makes with the positive horizontal is checked, and
+    if it falls within ``margin`` of a given cardinal direction, i.e. up, down, left, or
+    right, then the vector is snapped to that cardinal direction, maintaining its
+    original magnitude.
+
+    Parameters
+    ----------
+    vector : mnt.Vector3D
+        The vector to potentially snap to a cardinal direction.
+    margin : float
+        The maximum angle ``vector`` can make with a cardinal direction and still be
+        snapped to it, in *radians*.
+
+    Returns
+    -------
+    Vector3D
+        The resultant vector.
+    """
+    vector_magnitude = np.linalg.norm(vector)
+    angle = np.arccos(np.dot(vector, np.array([1, 0, 0])) / vector_magnitude)
+
+    vector_within_margin_of_cardinal_direction = (angle + margin) % (
+        np.pi / 2
+    ) <= 2 * margin
+    if vector_within_margin_of_cardinal_direction:
+        abs_max_index = np.argmax(np.abs(vector))
+        cardinalised_vector = np.zeros_like(vector)
+        # Flip the direction of the vector if necessary (i.e. if it's pointing left or
+        # down)
+        cardinalised_vector[abs_max_index] = vector_magnitude * np.sign(
+            vector[abs_max_index]
+        )
+        return cardinalised_vector
+
+    return vector
---
+++ tests/test_utils.py
@@ -0,0 +1,126 @@
+import copy
+
+import manim.typing as mnt
+import manim_eng._utils as utils
+import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize(
+    ("vector", "expected"),
+    [
+        pytest.param([1, 0, 0], [1, 0, 0], id="unit vector doesn't change"),
+        pytest.param([1, 1, 0], [0.70710678, 0.70710678, 0], id="non-unit vector"),
+    ],
+)
+def test_normalised(vector: list[float], expected: list[float]) -> None:
+    vector = np.array(vector)  # type: ignore[assignment]
+    vector_original = copy.deepcopy(vector)
+    expected = np.array(expected)  # type: ignore[assignment]
+
+    result = utils.normalised(vector)
+
+    assert np.allclose(result, expected, rtol=0.001)
+    assert np.all(vector == vector_original)
+
+
+@pytest.mark.parametrize(
+    ("vector", "tolerance", "expected"),
+    [
+        pytest.param([1, 5, 0.2], 0.1, [1, 5, 0.2], id="no action necessary"),
+        pytest.param([1, 2, 0.1], 0.1, [1, 2, 0], id="action just barely necessary"),
+        pytest.param([0.01, 5, 0.005], 0.1, [0, 5, 0], id="both values zeroed"),
+        pytest.param([4, 10, 5], 15, [0, 0, 0], id="all values zeroed"),
+    ],
+)
+def test_small_values_zeroed_out(
+    vector: list[float],
+    tolerance: float,
+    expected: list[float],
+) -> None:
+    vector = np.array(vector)  # type: ignore[assignment]
+    vector_original = copy.deepcopy(vector)
+    expected = np.array(expected)  # type: ignore[assignment]
+
+    result = utils.small_values_zeroed_out(vector, tolerance)
+
+    assert np.all(result == expected)
+    assert np.all(vector == vector_original)
+
+
+STANDARD_MARGIN = np.deg2rad(5)
+
+
+@pytest.mark.parametrize(
+    ("vector", "margin", "expected"),
+    [
+        pytest.param([1, 1, 0], STANDARD_MARGIN, [1, 1, 0], id="no action necessary"),
+        pytest.param(
+            [9.99048222, 0.43619387, 0],
+            STANDARD_MARGIN,
+            [10, 0, 0],
+            id="snapped vectors maintain magnitude",
+        ),
+        pytest.param(
+            [0.9961947, 0.08715574, 0],
+            STANDARD_MARGIN,
+            [1, 0, 0],
+            id="on positive margin (right)",
+        ),
+        pytest.param(
+            [0.9961947, -0.08715574, 0],
+            STANDARD_MARGIN,
+            [1, 0, 0],
+            id="on negative margin (right)",
+        ),
+        pytest.param(
+            [-0.9961947, 0.08715574, 0],
+            STANDARD_MARGIN,
+            [-1, 0, 0],
+            id="on negative margin (left)",
+        ),
+        pytest.param(
+            [-0.9961947, -0.08715574, 0],
+            STANDARD_MARGIN,
+            [-1, 0, 0],
+            id="on positive margin (left)",
+        ),
+        pytest.param(
+            [0.08715574, 0.9961947, 0],
+            STANDARD_MARGIN,
+            [0, 1, 0],
+            id="on negative margin (up)",
+        ),
+        pytest.param(
+            [-0.08715574, 0.9961947, 0],
+            STANDARD_MARGIN,
+            [0, 1, 0],
+            id="on positive margin (up)",
+        ),
+        pytest.param(
+            [-0.08715574, -0.9961947, 0],
+            STANDARD_MARGIN,
+            [0, -1, 0],
+            id="on negative margin (down)",
+        ),
+        pytest.param(
+            [0.08715574, -0.9961947, 0],
+            STANDARD_MARGIN,
+            [0, -1, 0],
+            id="on positive margin (down)",
+        ),
+    ],
+)
+def test_cardinalised(
+    vector: list[float],
+    margin: float,
+    expected: list[float],
+) -> None:
+    vector = np.array(vector)  # type: ignore[assignment]
+    vector_original = copy.deepcopy(vector)
+    expected = np.array(expected)  # type: ignore[assignment]
+
+    result = utils.cardinalised(vector, margin)
+
+    assert np.allclose(result, expected, rtol=0.00001)
+    assert np.all(vector == vector_original)
