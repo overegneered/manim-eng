c87dffeb33e0692474a22cadd652f7fcaa7d2cf0
overegneered <git@egneer.ing>
2024-08-23 15:07:37 +0100

    #2 Fix tests
    The refactors of the previous commits broke some tests and made others impossible (as they relied on implementation details of the Component class that had been moved). These have been fixed, and tests added for Mark and Markable to test their functionality (some of which was tested in test_component.py when that functionality was in Component).

--- tests/test_component.py
+++ tests/test_component.py
@@ -22,40 +22,15 @@ def test_set_label_no_existing_label(dummy_component: Component) -> None:
     dummy_component.set_label(label)
 
     assert dummy_component._label.tex_strings == [label]
-    assert dummy_component._marks.submobjects == [dummy_component._label]
 
 
 def test_set_label_existing_label(dummy_component: Component) -> None:
-    dummy_component._label = Mark(
-        dummy_component._label_anchor, dummy_component._centre_anchor
-    ).set_text("old")
-    dummy_component._marks.submobjects = [dummy_component._label]
+    dummy_component.set_label("old")
     new_label_text = "new"
 
     dummy_component.set_label(new_label_text)
 
     assert dummy_component._label.tex_strings == [new_label_text]
-    assert dummy_component._marks.submobjects == [dummy_component._label]
-
-
-def test_clear_label_label_exists(dummy_component: Component) -> None:
-    dummy_component._label = Mark(
-        dummy_component._label_anchor, dummy_component._centre_anchor
-    ).set_text("old")
-    dummy_component._marks.submobjects = [dummy_component._label]
-
-    dummy_component.clear_label()
-
-    assert dummy_component._marks.submobjects == []
-
-
-def test_clear_label_label_does_not_exist(dummy_component: Component) -> None:
-    dummy_component._marks.remove = mock.MagicMock(mn.VGroup)
-
-    dummy_component.clear_label()
-
-    assert dummy_component._marks.submobjects == []
-    dummy_component._marks.remove.assert_not_called()
 
 
 def test_set_annotation_no_existing_annotation(dummy_component: Component) -> None:
@@ -64,54 +39,27 @@ def test_set_annotation_no_existing_annotation(dummy_component: Component) -> No
     dummy_component.set_annotation(annotation)
 
     assert dummy_component._annotation.tex_strings == [annotation]
-    assert dummy_component._marks.submobjects == [dummy_component._annotation]
 
 
 def test_set_annotation_existing_annotation(dummy_component: Component) -> None:
-    dummy_component._annotation = Mark(
-        dummy_component._annotation_anchor, dummy_component._centre_anchor
-    ).set_text("old")
-    dummy_component._marks.submobjects = [dummy_component._annotation]
+    dummy_component.set_annotation("old")
     new_annotation_text = "new"
 
     dummy_component.set_annotation(new_annotation_text)
 
     assert dummy_component._annotation.tex_strings == [new_annotation_text]
-    assert dummy_component._marks.submobjects == [dummy_component._annotation]
-
-
-def test_clear_annotation_annotation_exists(dummy_component: Component) -> None:
-    dummy_component._annotation = Mark(
-        dummy_component._annotation_anchor, dummy_component._centre_anchor
-    ).set_text("old")
-    dummy_component._marks.submobjects = [dummy_component._annotation]
-
-    dummy_component.clear_annotation()
-
-    assert dummy_component._marks.submobjects == []
-
-
-def test_clear_annotation_annotation_does_not_exist(dummy_component: Component) -> None:
-    dummy_component._marks.remove = mock.MagicMock(mn.VGroup)
-
-    dummy_component.clear_annotation()
-
-    assert dummy_component._marks.submobjects == []
-    dummy_component._marks.remove.assert_not_called()
 
 
 def test_label_via_constructor_argument_works() -> None:
     dummy_component = DummyComponent(label="R")
 
     assert dummy_component._label.tex_strings == ["R"]
-    assert dummy_component._marks.submobjects == [dummy_component._label]
 
 
 def test_annotation_via_constructor_argument_works() -> None:
     dummy_component = DummyComponent(annotation=r"12 \Omega")
 
     assert dummy_component._annotation.tex_strings == [r"12 \Omega"]
-    assert dummy_component._marks.submobjects == [dummy_component._annotation]
 
 
 def test_label_and_annotation_via_constructor_argument_works() -> None:
@@ -119,15 +67,3 @@ def test_label_and_annotation_via_constructor_argument_works() -> None:
 
     assert dummy_component._label.tex_strings == ["Z"]
     assert dummy_component._annotation.tex_strings == [r"(2 + j4) \,\Omega"]
-    # Using a set means that order doesn't matter. We only care that it is exactly these
-    # two objects in _marks, we don't care what order they're in.
-    assert set(dummy_component._marks.submobjects) == {
-        dummy_component._label,
-        dummy_component._annotation,
-    }
-
-
-def test_supplying_neither_label_nor_annotation_via_constructor_argument_does_nothing(
-    dummy_component: DummyComponent,
-) -> None:
-    assert len(dummy_component._marks.submobjects) == 0
---
+++ tests/test_markable.py
@@ -0,0 +1,55 @@
+from unittest import mock
+
+import manim as mn
+import pytest
+from manim_eng.components._component.mark import Mark, Markable
+
+
+class SubclassesMarkable(Markable):
+    def __init__(self) -> None:
+        super().__init__()
+        self.mark = mock.MagicMock(Mark)
+
+
+@pytest.fixture()
+def markable_dummy() -> SubclassesMarkable:
+    return SubclassesMarkable()
+
+
+def test_set_mark_not_already_added(markable_dummy: SubclassesMarkable) -> None:
+    label = "A"
+
+    with mock.patch.object(mn.VGroup, "add") as patched_add:
+        markable_dummy._set_mark(markable_dummy.mark, label)
+
+        patched_add.assert_called_once()
+    markable_dummy.mark.set_text.assert_called_once_with(label)
+
+
+def test_set_mark_already_added(markable_dummy: SubclassesMarkable) -> None:
+    label_old, label_new = "B", "C"
+    markable_dummy._set_mark(markable_dummy.mark, label_old)
+
+    with mock.patch.object(mn.VGroup, "add") as patched_add:
+        markable_dummy._set_mark(markable_dummy.mark, label_new)
+
+        patched_add.assert_not_called()
+    markable_dummy.mark.set_text.assert_has_calls(
+        [mock.call(label_old), mock.call(label_new)]
+    )
+
+
+def test_clear_mark_mark_present(markable_dummy: SubclassesMarkable) -> None:
+    markable_dummy._set_mark(markable_dummy.mark, "D")
+
+    with mock.patch.object(mn.VGroup, "remove") as patched_remove:
+        markable_dummy._clear_mark(markable_dummy.mark)
+
+        patched_remove.assert_called_once_with(markable_dummy.mark)
+
+
+def test_clear_mark_mark_not_present(markable_dummy: SubclassesMarkable) -> None:
+    with mock.patch.object(mn.VGroup, "remove") as patched_remove:
+        markable_dummy._clear_mark(markable_dummy.mark)
+
+        patched_remove.assert_not_called()
