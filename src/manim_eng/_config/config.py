294fecd0144e34843800e51a17246fbf4a27b2bc
overegneered <git@egneer.ing>
2024-08-26 18:49:24 +0100

    #4 Add support and tests for loading colours

--- src/manim_eng/_config/config.py
+++ src/manim_eng/_config/config.py
@@ -1,14 +1,105 @@
 import dataclasses as dc
+import re
 from collections import defaultdict
 from typing import Any, Self
 
 import manim as mn
 import numpy as np
 
+STRING_TO_MANIM_COLOUR = {
+    "white": mn.WHITE,
+    "gray_a": mn.GRAY_A,
+    "grey_a": mn.GREY_A,
+    "gray_b": mn.GRAY_B,
+    "grey_b": mn.GREY_B,
+    "gray_c": mn.GRAY_C,
+    "grey_c": mn.GREY_C,
+    "gray_d": mn.GRAY_D,
+    "grey_d": mn.GREY_D,
+    "gray_e": mn.GRAY_E,
+    "grey_e": mn.GREY_E,
+    "black": mn.BLACK,
+    "lighter_gray": mn.LIGHTER_GRAY,
+    "lighter_grey": mn.LIGHTER_GREY,
+    "light_gray": mn.LIGHT_GRAY,
+    "light_grey": mn.LIGHT_GREY,
+    "gray": mn.GRAY,
+    "grey": mn.GREY,
+    "dark_gray": mn.DARK_GRAY,
+    "dark_grey": mn.DARK_GREY,
+    "darker_gray": mn.DARKER_GRAY,
+    "darker_grey": mn.DARKER_GREY,
+    "blue_a": mn.BLUE_A,
+    "blue_b": mn.BLUE_B,
+    "blue_c": mn.BLUE_C,
+    "blue_d": mn.BLUE_D,
+    "blue_e": mn.BLUE_E,
+    "pure_blue": mn.PURE_BLUE,
+    "blue": mn.BLUE,
+    "dark_blue": mn.DARK_BLUE,
+    "teal_a": mn.TEAL_A,
+    "teal_b": mn.TEAL_B,
+    "teal_c": mn.TEAL_C,
+    "teal_d": mn.TEAL_D,
+    "teal_e": mn.TEAL_E,
+    "teal": mn.TEAL,
+    "green_a": mn.GREEN_A,
+    "green_b": mn.GREEN_B,
+    "green_c": mn.GREEN_C,
+    "green_d": mn.GREEN_D,
+    "green_e": mn.GREEN_E,
+    "pure_green": mn.PURE_GREEN,
+    "green": mn.GREEN,
+    "yellow_a": mn.YELLOW_A,
+    "yellow_b": mn.YELLOW_B,
+    "yellow_c": mn.YELLOW_C,
+    "yellow_d": mn.YELLOW_D,
+    "yellow_e": mn.YELLOW_E,
+    "yellow": mn.YELLOW,
+    "gold_a": mn.GOLD_A,
+    "gold_b": mn.GOLD_B,
+    "gold_c": mn.GOLD_C,
+    "gold_d": mn.GOLD_D,
+    "gold_e": mn.GOLD_E,
+    "gold": mn.GOLD,
+    "red_a": mn.RED_A,
+    "red_b": mn.RED_B,
+    "red_c": mn.RED_C,
+    "red_d": mn.RED_D,
+    "red_e": mn.RED_E,
+    "pure_red": mn.PURE_RED,
+    "red": mn.RED,
+    "maroon_a": mn.MAROON_A,
+    "maroon_b": mn.MAROON_B,
+    "maroon_c": mn.MAROON_C,
+    "maroon_d": mn.MAROON_D,
+    "maroon_e": mn.MAROON_E,
+    "maroon": mn.MAROON,
+    "purple_a": mn.PURPLE_A,
+    "purple_b": mn.PURPLE_B,
+    "purple_c": mn.PURPLE_C,
+    "purple_d": mn.PURPLE_D,
+    "purple_e": mn.PURPLE_E,
+    "purple": mn.PURPLE,
+    "pink": mn.PINK,
+    "light_pink": mn.LIGHT_PINK,
+    "orange": mn.ORANGE,
+    "light_brown": mn.LIGHT_BROWN,
+    "dark_brown": mn.DARK_BROWN,
+    "gray_brown": mn.GRAY_BROWN,
+    "grey_brown": mn.GREY_BROWN,
+    "logo_white": mn.LOGO_WHITE,
+    "logo_green": mn.LOGO_GREEN,
+    "logo_blue": mn.LOGO_BLUE,
+    "logo_red": mn.LOGO_RED,
+    "logo_black": mn.LOGO_BLACK,
+}
+
 
 class ConfigBase:
     """Base class for manim-eng configuration classes."""
 
+    # TODO: #4 make this be able to handle colours
     def load_from_dict(
         self, dictionary: dict[str, Any], table_prefix: str = ""
     ) -> Self:
@@ -65,13 +156,36 @@ class ConfigBase:
                 )
                 continue
 
+            if isinstance(current_value, mn.ManimColor):
+                if isinstance(value, mn.ManimColor):
+                    setattr(self, key, value)
+                    continue
+                if not isinstance(value, str):
+                    raise ValueError(
+                        f"Invalid type in manim-eng configuration for key "
+                        f"`{table_prefix}{key}`: "
+                        f"{self._get_toml_type_from_python_variable(value)} "
+                        f"(expected string)"
+                    )
+                if value not in STRING_TO_MANIM_COLOUR:
+                    if not re.match(r"^#[0-9A-Fa-f]{6}$", value):
+                        raise ValueError(
+                            f"Invalid colour in manim-eng configuration "
+                            f"for key `{table_prefix}{key}`: "
+                            f"{value}"
+                        )
+                    setattr(self, key, mn.ManimColor(value))
+                    continue
+                setattr(self, key, STRING_TO_MANIM_COLOUR[value])
+                continue
+
             if not isinstance(value, type(current_value)):
                 raise ValueError(
                     f"Invalid type in manim-eng configuration for key "
                     f"`{table_prefix}{key}`: "
-                    f"{self.get_toml_type_from_python_variable(value)} "
+                    f"{self._get_toml_type_from_python_variable(value)} "
                     f"(expected "
-                    f"{self.get_toml_type_from_python_variable(current_value)}"
+                    f"{self._get_toml_type_from_python_variable(current_value)}"
                     f")"
                 )
 
@@ -79,8 +193,23 @@ class ConfigBase:
 
         return self
 
+    def as_dict(self) -> dict[str, Any]:
+        """Return this configuration as a dictionary.
+
+        Returns
+        -------
+        dict[str, Any]
+            This configuration as a dictionary, with subconfigurations being added as
+            subdictionaries.
+        """
+        dictionary = self.__dict__
+        for key, value in dictionary.items():
+            if isinstance(value, ConfigBase):
+                dictionary[key] = value.as_dict()
+        return dictionary
+
     @staticmethod
-    def get_toml_type_from_python_variable(variable: Any) -> str:
+    def _get_toml_type_from_python_variable(variable: Any) -> str:
         """Return the TOML type a Python variable would be stored as.
 
         Returns the TOML type that a Python value would've had if it were read from a
@@ -100,6 +229,7 @@ class ConfigBase:
                 "bool": "boolean",
                 "list": "array",
                 "dict": "table",
+                "ManimColor": "string",
             },
         )
         return conversion_table[type_name]
