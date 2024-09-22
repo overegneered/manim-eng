5fa70ce142b3affac095c9103c488aa6708d52c1
overegneered <git@egneer.ing>
2024-09-22 13:21:46 +0100

    Move dummy component to new test_utils package
    This is because the tests for the circuit class will wish to use the dummy component.
    
    The terminal names of the dummy component were also changed to be the more sensible and predictable 'terminal_1' and 'terminal_2'.

--- tests/component_test.py
+++ tests/component_test.py
@@ -1,33 +1,7 @@
-from typing import Any
-
-import manim as mn
 import pytest
 from manim_eng._base.component import Component
-from manim_eng._base.terminal import Terminal
 
-
-class DummyComponent(Component):
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        terminal = Terminal(mn.RIGHT, mn.RIGHT)
-        other_terminal = Terminal(mn.LEFT, mn.LEFT)
-        self.not_a_terminal = 3
-        super().__init__([terminal, other_terminal], *args, **kwargs)
-
-    def _construct(self) -> None:
-        pass
-
-    @property
-    def terminal(self) -> Terminal:
-        return self.terminals[0]
-
-    @property
-    def other_terminal(self) -> Terminal:
-        return self.terminals[1]
-
-
-@pytest.fixture()
-def dummy_component() -> Component:
-    return DummyComponent()
+from .test_utils.dummy_component import DummyComponent
 
 
 def test_set_label_no_existing_label(dummy_component: Component) -> None:
@@ -85,28 +59,28 @@ def test_label_and_annotation_via_constructor_argument_works() -> None:
 
 def test_voltage_processes_terminals_correctly(dummy_component: DummyComponent) -> None:
     voltage_1 = dummy_component.voltage(
-        dummy_component.terminal,
-        dummy_component.other_terminal,
+        dummy_component.terminal_1,
+        dummy_component.terminal_2,
         "V",
     )
-    voltage_2 = dummy_component.voltage("terminal", dummy_component.other_terminal, "V")
-    voltage_3 = dummy_component.voltage(dummy_component.terminal, "other_terminal", "V")
-    voltage_4 = dummy_component.voltage("terminal", "other_terminal", "V")
+    voltage_2 = dummy_component.voltage("terminal_1", dummy_component.terminal_2, "V")
+    voltage_3 = dummy_component.voltage(dummy_component.terminal_1, "terminal_2", "V")
+    voltage_4 = dummy_component.voltage("terminal_1", "terminal_2", "V")
 
-    assert voltage_1.from_terminal == dummy_component.terminal
-    assert voltage_1.to_terminal == dummy_component.other_terminal
-    assert voltage_2.from_terminal == dummy_component.terminal
-    assert voltage_2.to_terminal == dummy_component.other_terminal
-    assert voltage_3.from_terminal == dummy_component.terminal
-    assert voltage_3.to_terminal == dummy_component.other_terminal
-    assert voltage_4.from_terminal == dummy_component.terminal
-    assert voltage_4.to_terminal == dummy_component.other_terminal
+    assert voltage_1.from_terminal == dummy_component.terminal_1
+    assert voltage_1.to_terminal == dummy_component.terminal_2
+    assert voltage_2.from_terminal == dummy_component.terminal_1
+    assert voltage_2.to_terminal == dummy_component.terminal_2
+    assert voltage_3.from_terminal == dummy_component.terminal_1
+    assert voltage_3.to_terminal == dummy_component.terminal_2
+    assert voltage_4.from_terminal == dummy_component.terminal_1
+    assert voltage_4.to_terminal == dummy_component.terminal_2
 
 
 def test_voltage_sets_component_it_is_called_on_as_avoid(
     dummy_component: DummyComponent,
 ) -> None:
-    voltage = dummy_component.voltage("terminal", "other_terminal", "V")
+    voltage = dummy_component.voltage("terminal_1", "terminal_2", "V")
 
     assert voltage.component_to_avoid == dummy_component
 
@@ -120,13 +94,13 @@ def test_voltage_errors_if_terminals_are_the_same(
     )
 
     with pytest.raises(ValueError, match=expected_message):
-        dummy_component.voltage(dummy_component.terminal, dummy_component.terminal)
+        dummy_component.voltage(dummy_component.terminal_1, dummy_component.terminal_1)
     with pytest.raises(ValueError, match=expected_message):
-        dummy_component.voltage("terminal", dummy_component.terminal)
+        dummy_component.voltage("terminal_1", dummy_component.terminal_1)
     with pytest.raises(ValueError, match=expected_message):
-        dummy_component.voltage(dummy_component.terminal, "terminal")
+        dummy_component.voltage(dummy_component.terminal_1, "terminal_1")
     with pytest.raises(ValueError, match=expected_message):
-        dummy_component.voltage("terminal", "terminal")
+        dummy_component.voltage("terminal_1", "terminal_1")
 
 
 def test_get_or_check_terminal_non_belonging_terminal() -> None:
@@ -136,7 +110,7 @@ def test_get_or_check_terminal_non_belonging_terminal() -> None:
     with pytest.raises(
         ValueError, match="Passed terminal does not belong to this component."
     ):
-        component._get_or_check_terminal(other_component.terminal)
+        component._get_or_check_terminal(other_component.terminal_1)
 
 
 def test_get_or_check_terminal_invalid_attribute(
@@ -159,12 +133,12 @@ def test_get_or_check_terminal_valid_attribute_not_a_terminal(
 
 
 def test_get_or_check_terminal_valid_terminal(dummy_component: DummyComponent) -> None:
-    result = dummy_component._get_or_check_terminal(dummy_component.terminal)
+    result = dummy_component._get_or_check_terminal(dummy_component.terminal_1)
 
-    assert result == dummy_component.terminal
+    assert result == dummy_component.terminal_1
 
 
 def test_get_or_check_terminal_valid_string(dummy_component: DummyComponent) -> None:
-    result = dummy_component._get_or_check_terminal("terminal")
+    result = dummy_component._get_or_check_terminal("terminal_1")
 
-    assert result == dummy_component.terminal
+    assert result == dummy_component.terminal_1
---
+++ tests/test_utils/dummy_component.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+import manim as mn
+import pytest
+from manim_eng._base.component import Component
+from manim_eng._base.terminal import Terminal
+
+
+class DummyComponent(Component):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        terminal_1 = Terminal(mn.RIGHT, mn.RIGHT)
+        terminal_2 = Terminal(mn.LEFT, mn.LEFT)
+        self.not_a_terminal = 3
+        super().__init__([terminal_1, terminal_2], *args, **kwargs)
+
+    def _construct(self) -> None:
+        pass
+
+    @property
+    def terminal_1(self) -> Terminal:
+        return self.terminals[0]
+
+    @property
+    def terminal_2(self) -> Terminal:
+        return self.terminals[1]
+
+
+@pytest.fixture()
+def dummy_component() -> Component:
+    return DummyComponent()
