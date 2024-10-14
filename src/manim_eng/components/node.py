67c86f0eed1312100ab3afbb0ff9f58638904312
overegneered <git@egneer.ing>
2024-10-15 00:14:16 +0100

    #18 Add ability for Nodes to have labels
    These labels can be autoplaced in the largest available space or their positions manually specified by the user.
    
    Also removed the ability to set annotations on nodes.

--- src/manim_eng/components/node.py
+++ src/manim_eng/components/node.py
@@ -59,11 +59,49 @@ class Node(Component):
         if self.autoblob:
             self.add_updater(self.__blob_updater)
 
+        self.remove(self._annotation_anchor)
+
     def _construct(self) -> None:
         super()._construct()
 
         self.__blob = _create_node_blob(self, self.open)
         self._body.add(self.__blob)
+
+    def set_label(
+        self, label: str, direction: mnt.Vector3D | float | None = None
+    ) -> Self:
+        """Set the label of the node, optionally specifying where it should be.
+
+        The ``direction`` parameter can be used to specify the position the label should
+        take. If it is left unspecified, the node will identify the most logical
+        position for it (the widest gap between terminals) and keep the label here, with
+        ties being broken by the uppermost position being favoured. This will continue
+        to happen as terminals are added/removed.
+
+        Parameters
+        ----------
+        label : str
+            The label to set. Takes a TeX math mode string.
+        direction : Vector3D | float | None
+            The direction in which to place the label. Can either be a direction vector
+            (``Vector3D``), an angle in radians (``float``), or ``None``, which
+            signifies that the label should be placed automatically.
+        """
+        self._reposition_label_anchor(direction)
+        super().set_label(label)
+        return self
+
+    def set_annotation(self, annotation: str) -> Self:
+        """Fails for nodes, as they do not have annotations."""
+        raise NotImplementedError(
+            "Monopoles have no annotation. Please use `.set_label()`."
+        )
+
+    def clear_annotation(self) -> Self:
+        """Fails for nodes, as they do not have annotations."""
+        raise NotImplementedError(
+            "Monopoles have no annotation. Please use `.clear_label()`."
+        )
 
     def get(self, direction: mnt.Vector3D | float) -> Terminal:
         """Get a terminal of the node in a given direction, creating it if necessary.
@@ -311,10 +349,93 @@ class Node(Component):
         )
         return visible_terminal_count > AUTOBLOBBING_BLOB_THRESHOLD
 
+    def _get_visible_terminal_angles(self) -> list[float]:
+        return sorted(
+            [
+                mn.angle_of_vector(terminal.direction)
+                for terminal in self.terminals
+                if terminal.is_visible()
+            ]
+        )
+
+    @staticmethod
+    def _midangles_of_largest_gaps_between_list_of_angles(
+        angles: list[float],
+    ) -> list[float]:
+        largest_gap = 0.0
+        midangles: list[float] = []
+        for start_angle, end_angle in zip(np.roll(angles, 1), angles):
+            if end_angle <= start_angle:
+                # This comparison occurs over the 'break' at ±pi, or there is only one
+                # element in `angles`. Either way, we get the behaviour we want by
+                # adding 2 pi
+                end_angle += 2 * np.pi  # noqa: PLW2901
+
+            gap = end_angle - start_angle
+            if gap < largest_gap and not np.isclose(gap, largest_gap):
+                continue
+
+            if gap > largest_gap:
+                largest_gap = gap
+                midangles = []
+
+            centre_angle = 0.5 * (start_angle + end_angle)
+            if centre_angle > np.pi:
+                centre_angle -= 2 * np.pi
+            midangles.append(centre_angle)
+
+        return sorted(midangles)
+
+    @staticmethod
+    def _topmost_angle_as_direction(angles: list[float]) -> mnt.Vector3D:
+        if len(angles) == 0:
+            return mn.UP
+
+        angles.sort(key=lambda x: np.sin(x), reverse=True)
+        angle = angles[0]
+        return mn.rotate_vector(mn.RIGHT, angle)
+
+    def _get_optimal_label_anchor_direction(self) -> mnt.Vector3D:
+        terminal_angles = self._get_visible_terminal_angles()
+        angles = self._midangles_of_largest_gaps_between_list_of_angles(terminal_angles)
+        return self._topmost_angle_as_direction(angles)
+
+    def _update_label_positioning_using_vector(self, direction: mnt.Vector3D) -> None:
+        position = self.get_center() + config_eng.symbol.node_radius * direction
+        self._label_anchor.move_to(position)
+        self._label.update()
+
+    def _reposition_label_anchor(self, direction: mnt.Vector3D | float | None) -> None:
+        if direction is None:
+            if self.__label_anchor_updater not in self.updaters:
+                self.add_updater(self.__label_anchor_updater)
+            self.update()
+        else:
+            if self.__label_anchor_updater in self.updaters:
+                self.remove_updater(self.__label_anchor_updater)
+            direction = self._get_normalised_direction(direction)
+            self._update_label_positioning_using_vector(direction)
+
     @staticmethod
     def __blob_updater(mobject: mn.Mobject) -> None:
         node = cast(Node, mobject)
         node._set_blob_visibility(node._should_be_visible())
+
+    @staticmethod
+    def __label_anchor_updater(mobject: mn.Mobject) -> None:
+        node = cast(Node, mobject)
+        new_direction = node._get_optimal_label_anchor_direction()
+        node._update_label_positioning_using_vector(new_direction)
+
+    @mn.override_animate(set_label)
+    def __animate_set_label(
+        self,
+        label: str,
+        direction: mnt.Vector3D | float | None = None,
+        anim_args: dict[str, Any] | None = None,
+    ) -> mn.Animation:
+        self._reposition_label_anchor(direction)
+        return self.animate(**anim_args)._set_mark(self._label, label).build()
 
 
 class OpenNode(Node):
