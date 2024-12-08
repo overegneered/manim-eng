22d797f851446881ef51ca3004139caa59820bd7
overegneered <git@egneer.ing>
2024-12-08 00:06:36 +0000

    #13 clean up markable.py pt 2 (fix tests)

--- tests/markable_test.py
+++ tests/markable_test.py
@@ -35,7 +35,7 @@ def test_set_mark_already_added(markable_dummy: SubclassesMarkable) -> None:
     with mock.patch.object(mn.VGroup, "add") as patched_add:
         markable_dummy._set_mark(markable_dummy.mark, label_new)
 
-        patched_add.assert_not_called()
+        patched_add.assert_called_once()
     markable_dummy.mark.set_text.assert_has_calls(
         [mock.call(label_old), mock.call(label_new)]
     )
@@ -51,17 +51,10 @@ def test_set_mark_with_value(markable_dummy: SubclassesMarkable) -> None:
     markable_dummy.mark.set_text.assert_called_once_with(value.to_latex())
 
 
-def test_clear_mark_mark_present(markable_dummy: SubclassesMarkable) -> None:
+def test_clear_mark(markable_dummy: SubclassesMarkable) -> None:
     markable_dummy._set_mark(markable_dummy.mark, "D")
 
     with mock.patch.object(mn.VGroup, "remove") as patched_remove:
         markable_dummy._clear_mark(markable_dummy.mark)
 
         patched_remove.assert_called_once_with(markable_dummy.mark)
-
-
-def test_clear_mark_mark_not_present(markable_dummy: SubclassesMarkable) -> None:
-    with mock.patch.object(mn.VGroup, "remove") as patched_remove:
-        markable_dummy._clear_mark(markable_dummy.mark)
-
-        patched_remove.assert_not_called()
