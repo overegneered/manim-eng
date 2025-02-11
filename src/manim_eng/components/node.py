a6b733962a0f5a6d3c6b10dcacc71c8554f1f495
overegneered <git@egneer.ing>
2025-02-11 21:09:17 +0000

    Remove removed method from Node
    The `._autoblob_if_autoblobbing()` method was removed, but a call to it was left in `.set_autoblobbing`. This has now been removed.

--- src/manim_eng/components/node.py
+++ src/manim_eng/components/node.py
@@ -252,7 +252,6 @@ class Node(Component):
             self.update()
         else:
             self.remove_updater(self.__blob_updater)
-        self._autoblob_if_autoblobbing()
         return self
 
     def enable_autoblobbing(self) -> Self:
